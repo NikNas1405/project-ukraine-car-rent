9c9b5b180fbd3d111cd3a79693c0d02416f64bf0
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2024-01-30 22:19:25 +0000

    Deploying to gh-pages from @ NikNas1405/project-ukraine-car-rent@511837cfc234fe2216302b4294c57a682ac2b1c1 🚀

---
+++ assets/ErrorPage-2e728181.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-3e32abd7.js";const e=()=>r.jsx("h2",{children:"404"});export{e as default};
---
+++ assets/HomePage-3a0dbd51.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-3e32abd7.js";const r=()=>e.jsx("h1",{children:"Home Page"});export{r as default};
